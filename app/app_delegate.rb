5239d0b620e9c103a74f949d5df478dbd39b3815
Naoya Makino <naoyamakino@gmail.com>
2012-11-19 12:57:13 -0800

    working on defining views and queries. I am not sure how to properly get documents for an argument to mapBlock.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -3,6 +3,7 @@ class AppDelegate
     @server = CouchTouchDBServer.sharedInstance
     raise "CouchTouchDBServer failed to start: #{@server.error}" if @server.error
     @database = @server.databaseNamed("my-database")
+    @design = @database.designDocumentWithName("my-design")
     error = Pointer.new(:object)
     unless @database.ensureCreated(error)
       raise "database failed to be created"
@@ -29,6 +30,20 @@ class AppDelegate
     query = @database.getAllDocuments
     query.limit = option[:limit] unless option[:limit].nil?
     query.descending = option[:descending] unless option[:descending].nil?
+    query.rows.each do |row|
+      puts "#{row.documentProperties.inspect}"
+    end
+  end
+
+  def defineViewByFirstName
+    doc = @database.untitledDocument #I don't know how to get a proper document
+    @design.defineViewNamed("by_first_name", mapBlock:lambda do |doc, emit|
+      first_name = doc.objectForKey("first_name")
+      emit(first_name, doc) unless first_name.nil?
+    end, version:"1.0")
+  end
+  def query_by_first_name
+    query = @design.queryViewNamed("by_first_name")
     query.rows.each do |row|
       puts "#{row.documentProperties.inspect}"
     end
