85ef91953494dbb98a6d7a7aea6f6f4c23536f20
naoyamakino <naoyamakino@gmail.com>
2012-11-18 20:44:48 -0800

    added AppDelegate.createDocument. TODO: how to make it run asynchronously

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,12 +1,24 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
-    server = CouchTouchDBServer.sharedInstance
-    raise "CouchTouchDBServer failed to start: #{server.error}" if server.error
-    database =server.databaseNamed("my-database")
+    @server = CouchTouchDBServer.sharedInstance
+    raise "CouchTouchDBServer failed to start: #{@server.error}" if @server.error
+    @database = @server.databaseNamed("my-database")
     error = Pointer.new(:object)
-    unless database.ensureCreated(error)
+    unless @database.ensureCreated(error)
       raise "database failed to be created"
     end
+
     true
+  end
+
+  #TODO how to make it run the operation asynchronously?
+  def createDocument(content)
+    doc = @database.untitledDocument
+    op = doc.putProperties(content)
+    if !op.wait
+      raise "couldn't save the new item"
+    else
+      puts "#{content.inspect} has been saved."
+    end
   end
 end
